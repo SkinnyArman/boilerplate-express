72f5f7127e6b03a69507dd771280454d030df185
Arman Amini <armanwithamini@gmail.com>
2024-03-29 01:55:12 -0700

    change "h" to "H"

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req,res)=> {
 })
 
 app.get("/json", (req,res) => {
-  let message = "hello json"
+  let message = "Hello json"
   if (process.env.MESSAGE_STYLE = "uppercase") {
     res.json({ "message": message.toUpperCase() })
   } else {
